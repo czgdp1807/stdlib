081ac9e5a4233ba29edc088a9b1c94aeee4cf152
Ondřej Čertík <ondrej@certik.us>
2023-12-22 10:06:58 -0700

    Compile and run

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -ex
 git clean -dfx
 
 FC=lfortran cmake .
-make || true
+make example_gamma || true
 cp src/*.mod example/specialfunctions_gamma
-make
-ctest
+make example_gamma
+./example/specialfunctions_gamma/example_gamma
