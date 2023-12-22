5770030bd06973eea4983821159d76a0cf8b5277
Ondřej Čertík <ondrej@certik.us>
2023-12-22 10:07:42 -0700

    Build without errors

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -ex
 git clean -dfx
 
 FC=lfortran cmake .
-make example_gamma || true
+make fortran_stdlib
 cp src/*.mod example/specialfunctions_gamma
 make example_gamma
 ./example/specialfunctions_gamma/example_gamma
