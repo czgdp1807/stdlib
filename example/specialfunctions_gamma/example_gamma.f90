edee18d855ee3c7227ab4b63db1b944263738764
Ondřej Čertík <ondrej@certik.us>
2023-12-22 10:19:25 -0700

    Uncomment more

--- example/specialfunctions_gamma/example_gamma.f90
+++ example/specialfunctions_gamma/example_gamma.f90
@@ -20,18 +20,18 @@ program example_gamma
   print *, mgamma(i)              !integer gives exact result
 ! 362880
 
-!  print *, mgamma(n)
+  print *, mgamma(n)
 ! 87178291200
 
-!  print *, gamma(x)              ! intrinsic function call
+  print *, gamma(x)              ! intrinsic function call
 ! 1.32934034
 
-!  print *, gamma(y)              ! intrinsic function call
+  print *, gamma(y)              ! intrinsic function call
 ! 8.8553433604540341
 
-!  print *, mgamma(z)
+  print *, mgamma(z)
 ! (0.988054395, 0.383354813)
 
-!  print *, mgamma(z1)
+  print *, mgamma(z1)
 ! (-2.78916032990983999E-005, 9.83164600163221218E-006)
 end program example_gamma
