649ee6d4862f9ea06fb2880a18978bb1d8a35dce
Ondřej Čertík <ondrej@certik.us>
2023-12-22 10:17:21 -0700

    Run gfortran too

--- build.sh
+++ build.sh
@@ -4,6 +4,13 @@ set -ex
 
 git clean -dfx
 
+FC=gfortran cmake .
+make fortran_stdlib
+make example_gamma
+./example/specialfunctions_gamma/example_gamma
+
+git clean -dfx
+
 FC=lfortran cmake .
 make fortran_stdlib
 cp src/*.mod example/specialfunctions_gamma
