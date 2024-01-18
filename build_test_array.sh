c53dea9fa01dba9fcb06feeba0ac6fa74d8b8594
Gagandeep Singh <gdp.1807@gmail.com>
2024-01-19 03:09:22 +0530

    Add utility script for building test/array

---
+++ build_test_array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+git clean -fdx
+
+FC=lfortran cmake .
+make fortran_stdlib
+cp src/*.mod test/array
+cd test/array
+lfortran ../../subprojects/test-drive/src/testdrive.F90 -c
+lfortran test_logicalloc.f90 --show-asr --no-indent
