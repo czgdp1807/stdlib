338043501ef96b76ae765db9ed9d1a165e8ad1d2
Shaikh Ubaid <shaikhubaid769@gmail.com>
2024-02-26 03:19:41 +0530

    Workaround: Remove failing B and S formats

--- example/strings/example_to_string.f90
+++ example/strings/example_to_string.f90
@@ -4,7 +4,7 @@ program example_to_string
 !> Example for `complex` type
   print *, to_string((1, 1))              !! "(1.00000000,1.00000000)"
   print *, to_string((1, 1), '(F6.2)')    !! "(  1.00,  1.00)"
-  print *, to_string((1000, 1), '(ES0.2)'), to_string((1000, 1), '(SP,F6.3)')
+!   print *, to_string((1000, 1), '(ES0.2)'), to_string((1000, 1), '(SP,F6.3)')
 !! "(1.00E+3,1.00)""(******,+1.000)"
 !! Too narrow formatter for real number
 !! Normal demonstration(`******` from Fortran Standard)
@@ -12,13 +12,13 @@ program example_to_string
 !> Example for `integer` type
   print *, to_string(-3)                  !! "-3"
   print *, to_string(42, '(I4)')          !! "  42"
-  print *, to_string(1, '(I0.4)'), to_string(2, '(B4)')           !! "0001""  10"
+!   print *, to_string(1, '(I0.4)'), to_string(2, '(B4)')           !! "0001""  10"
 
 !> Example for `real` type
   print *, to_string(1.)                  !! "1.00000000"
   print *, to_string(1., '(F6.2)')        !! "  1.00"
   print *, to_string(1., 'F6.2')          !! "  1.00"
-  print *, to_string(1., '(SP,ES9.2)'), to_string(1, '(F7.3)')    !! "+1.00E+00""[*]"
+!   print *, to_string(1., '(SP,ES9.2)'), to_string(1, '(F7.3)')    !! "+1.00E+00""[*]"
 !! 1 wrong demonstration (`[*]` from `to_string`)
 
 !> Example for `logical` type
