f4b86a08f884b668e2fb310fc32c585f7a2e5313
Pranavchiku <goswami.4@iitj.ac.in>
2025-03-24 19:59:01 +0530

    XX: convert submodule to stdlib_strings module

--- src/stdlib_ansi.f90
+++ src/stdlib_ansi.f90
@@ -17,6 +17,8 @@ module stdlib_ansi
 
     public :: to_string, operator(+), operator(//)
 
+    character, parameter :: esc = achar(27), chars(0:9) = &
+        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
     !> Container for terminal escape code
@@ -155,5 +157,35 @@ module stdlib_ansi
             type(string_type) :: str
         end function concat_right_str
     end interface operator(//)
+
+contains
+
+    !> Transform a color code into an actual ANSI escape sequence
+    pure function to_string_ansi_code(code) result(str)
+        !> Color code to be used
+        type(ansi_code), intent(in) :: code
+        !> ANSI escape sequence representing the color code
+        character(len=:), allocatable :: str
+
+        if (anycolor(code)) then
+            str = esc // "[0"  ! Always reset the style
+            if (code%style > 0 .and. code%style < 10) str = str // ";" // chars(code%style)
+            if (code%fg >= 0 .and. code%fg < 10) str = str // ";3" // chars(code%fg)
+            if (code%bg >= 0 .and. code%bg < 10) str = str // ";4" // chars(code%bg)
+            str = str // "m"
+        else
+            str = ""
+        end if
+    end function to_string_ansi_code
+
+    !> Check whether the code describes any color / style or is just a stub
+    pure function anycolor(code)
+        !> Escape sequence
+        type(ansi_code), intent(in) :: code
+        !> Any color / style is active
+        logical :: anycolor
+
+        anycolor = code%fg >= 0 .or. code%bg >= 0 .or. code%style >= 0
+    end function anycolor
 
 end module stdlib_ansi
