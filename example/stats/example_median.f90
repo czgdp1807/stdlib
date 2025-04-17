02230ca186f22e2b96a1cb25f4255e0f7b042e47
Pranavchiku <goswami.4@iitj.ac.in>
2025-04-17 17:01:12 +0530

    XX: convert submodule stats median to module and consequent changes

--- example/stats/example_median.f90
+++ example/stats/example_median.f90
@@ -1,5 +1,5 @@
 program example_median
-  use stdlib_stats, only: median
+  use stdlib_stats_median, only: median
   implicit none
   real :: x(1:6) = [1., 2., 3., 4., 5., 6.]
   real :: y(1:2, 1:3) = reshape([1., 2., 3., 4., 5., 6.], [2, 3])
