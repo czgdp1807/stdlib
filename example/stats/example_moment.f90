311792bae55117d0ca5eb7f8800bfb8a26d60eb9
Pranavchiku <goswami.4@iitj.ac.in>
2025-04-18 11:22:04 +0530

    XX: port changes from submodule to module stdlib_stats_moment and consequent changes

--- example/stats/example_moment.f90
+++ example/stats/example_moment.f90
@@ -1,5 +1,6 @@
 program example_moment
-  use stdlib_stats, only: moment
+  use ieee_arithmetic, only: ieee_is_nan
+  use stdlib_stats_moment, only: moment
   implicit none
   real :: x(1:6) = [1., 2., 3., 4., 5., 6.]
   real :: y(1:2, 1:3) = reshape([1., 2., 3., 4., 5., 6.], [2, 3])
