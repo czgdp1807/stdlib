321f96ad5a67e0858886e036517f6a5087790c49
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-05-12 01:58:46 +0530

    XX : Add hasher_fun function into submodule stdlib_hashmap_chaining (Issue #7205)

--- src/stdlib_hashmap_chaining.f90
+++ src/stdlib_hashmap_chaining.f90
@@ -30,6 +30,14 @@ submodule(stdlib_hashmaps) stdlib_hashmap_chaining
 
     character(len=*), parameter :: submodule_name = "STDLIB_HASHMAP_CHAINING"
 
+    abstract interface
+        pure function hasher_fun_temporary( key )  result(hash_value)
+            import key_type, int_hash
+            type(key_type), intent(in)    :: key
+            integer(int_hash)             :: hash_value
+        end function hasher_fun_temporary
+    end interface
+
     interface expand_slots
 !! Version: Experimental
 !!
@@ -434,7 +442,7 @@ contains
 !!             greater than max_bits
 !
         class(chaining_hashmap_type), intent(out)  :: map
-        procedure(hasher_fun)                      :: hasher
+        procedure(hasher_fun_temporary)                      :: hasher
         integer, intent(in), optional              :: slots_bits
         integer(int32), intent(out), optional      :: status
 
@@ -681,7 +689,7 @@ contains
 !!     hasher the hasher function to be used for the table
 !
         class(chaining_hashmap_type), intent(inout) :: map
-        procedure(hasher_fun)                       :: hasher
+        procedure(hasher_fun_temporary)                       :: hasher
 
         integer(int_hash)  :: hash_val
         integer(int_index) :: i
