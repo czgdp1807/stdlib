572d8eb4581793be09c062b6e82926d826413d49
AnshMehta1 <ansh.mehta522007@gmail.com>
2025-05-31 00:07:36 +0530

    XX : Directly use fnv_1_hasher instead of procedure pointer hasher of Struct hashmap_type in stdlib_hashmap_open.f90 to remove deserialization error (Issue #7478)

--- src/stdlib_hashmap_open.f90
+++ src/stdlib_hashmap_open.f90
@@ -360,7 +360,7 @@ contains
         integer(int_index) :: &
             offset
 
-        hash_val = map % hasher( key )
+        hash_val = fnv_1_hasher( key )
 
         if ( map % probe_count > inmap_probe_factor * map % call_count .or. &
              map % num_entries >= load_factor *                             &
@@ -544,7 +544,7 @@ contains
         integer(int_index) :: inmap, offset, test_slot
         character(*), parameter :: procedure = 'MAP_ENTRY'
 
-        hash_val = map % hasher( key )
+        hash_val = fnv_1_hasher( key )
 
         if ( map % probe_count > map_probe_factor * map % call_count .or.   &
              map % num_entries >= load_factor * size( map % slots,          &
@@ -686,7 +686,7 @@ contains
 
         do i=1, map % num_entries + map % num_free
             if ( .not. associated( map % inverse(i) % target ) ) cycle
-            hash_val = map % hasher( map % inverse(i) % target % key )
+            hash_val = fnv_1_hasher( map % inverse(i) % target % key )
             map % inverse(i) % target % hash_val = hash_val
             base_slot = fibonaccI_hash( hash_val, map % nbits )
             offset = 0
